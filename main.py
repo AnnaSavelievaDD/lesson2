064811a6a802e2507e1c5cce021d939f1784bedf
AnnaSavelievaDD <117804678+AnnaSavelievaDD@users.noreply.github.com>
2024-09-15 15:14:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+count_of_homeworks = "12"
+numbers_of_hours = "1,5"
+name = 'Python'
+print(1.5/12)
+print('курс', name,'всего задач:',count_of_homeworks, 'затрачено часов:',numbers_of_hours, 'среднее время выполнения',(1.5/12))
