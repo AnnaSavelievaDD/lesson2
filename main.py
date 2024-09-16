9cde4a518ff5eca014b4f7df8d69b3afbc6bb7ac
AnnaSavelievaDD <117804678+AnnaSavelievaDD@users.noreply.github.com>
2024-09-16 13:03:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-count_of_homeworks = "12"
-numbers_of_hours = "1,5"
+count_of_homeworks = 12
+numbers_of_hours = 1.5
 name = 'Python'
-print(1.5/12)
-print('курс', name,'всего задач:',count_of_homeworks, 'затрачено часов:',numbers_of_hours, 'среднее время выполнения',(1.5/12))
+print(numbers_of_hours / count_of_homeworks)
+print('курс', name,'всего задач:',count_of_homeworks, 'затрачено часов:',numbers_of_hours, 'среднее время выполнения',(numbers_of_hours / count_of_homeworks))
