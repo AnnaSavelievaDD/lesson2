ca492d9d2d7fd691ec57ea14c364608cab668f02
AnnaSavelievaDD <117804678+AnnaSavelievaDD@users.noreply.github.com>
2024-09-23 17:45:51 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Anna': 1982, 'Serafima': 2013, 'Tihon': 2012, 'Gleb': 2017}
+print (my_dict)
+print (my_dict.get('Anna'))
+print (my_dict.get('Matvey', 'Такого ключа нет'))
+my_dict.update({'Sergey': 1970, 'Svetlana': 1984})
+a=my_dict.pop('Sergey')
+print (a)
+print (my_dict)
+my_set={'bread', 2024, 'fish', 'meat', 3, 5, 'bread', 3, 'egg', 'egg', 2024}
+print (my_set)
+print (my_set.add('septemder'))
+print (my_set.remove(3))
+print (my_set)
